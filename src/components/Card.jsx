87840292cb35579eded6d70b1f2fd1df97345ced
luke-h1 <luke.howsam@yahoo.com>
2020-10-14 18:07:15 +0100

    setup linting config, start on Navbar.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'; 
+import './Card.css'; 
+
+const Card = () => { 
+  return (
+    <Fragment>
+      <h1>Hello!</h1>
+    </Fragment>
+  )
+}
+export default Card;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { Fragment, useState } from 'react';
+import './Navbar.css'; 
+import { Link } from 'react-router-dom'
+import { BsFillHouseFill } from "react-icons/bs";
+import { FaTimes, FaBars } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <Fragment>
+      <nav className="navbar">
+        <div className='navbar-container container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu} onBlur={closeMobileMenu}>
+          <BsFillHouseFill className='iconStyles' /> 
+          <p className='lead'>This is the title</p>
+          </Link>
+          <div className="menu-icon" onClick={handleClick} onBlur={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to='/' className={navLinkStyles} onClick={closeMobileMenu} onBlur={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/houses' className={navLinkStyles} onClick={closeMobileMenu} onBlur={closeMobileMenu}>
+                Houses
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/apartments' className={navLinkStyles} onClick={closeMobileMenu} onBlur={closeMobileMenu}>
+                Apartments
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Fragment>
+  ); 
+}
+
+Navbar.defaultProps = { 
+  title: 'Lakewood real-estate',
+  iconStyles: 'navbar-icon',
+  navLinkStyles: 'nav-links'
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+
+export default Navbar
